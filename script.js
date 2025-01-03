fc07ddc92a45a9baeec86a77e62f91ec1b34dd4f
devidutta-07 <deviduttame32@gmail.com>
2025-01-03 13:52:58 +0530

    1st Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Mobile menu toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Close mobile menu when clicking outside
+document.addEventListener('click', (e) => {
+    if (!e.target.closest('.nav')) {
+        navLinks.classList.remove('active');
+    }
+});
+
